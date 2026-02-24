618c2dc296f0c5d6833d7c133cd5d1391483f0b2
Andresinho <rojassanchezedwinandres@gmail.com>
2026-02-23 21:01:30 -0500

    En este commit definimos nuestro modelo interno y tipos de los pokemon siendo lo principal.

---
+++ src/types/pokemon.ts
@@ -0,0 +1,14 @@
+export interface Pokemon {
+    name: string;
+    url: string;
+    discovered: boolean;
+    image?: string;
+    description: string;
+}
+
+export interface PokemonListResponse {
+    results: {
+        name: string;
+        url: string;
+    }[];
+}
